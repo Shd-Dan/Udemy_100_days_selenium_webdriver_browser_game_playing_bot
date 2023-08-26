9e3eda0a7c7e4ae407f3a88709e45ec832b8f4dc
Shd-Dan <shd.cs.ch@gmail.com>
2023-08-26 20:35:30 +0600

    asd

--- cookie_clicker_project.py
+++ cookie_clicker_project.py
@@ -1,26 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.alert import Alert
 import time
 
 driver_path = "C:\Development\chromedriver"
 driver = webdriver.Chrome()
+driver.maximize_window()
 driver.get("https://orteil.dashnet.org/cookieclicker/")
-actions = ActionChains(driver)
+driver.implicitly_wait(4)
 
-# pop-up selecting english lan
-# language_frame = driver.find_element(By.ID, "promptAnchor")
-# driver.switch_to.frame(language_frame)
-# english = driver.find_element(By.ID, "langSelect-EN")
-# english.click()
-# alert = driver.switch_to.alert
-# alert.accept()
+# language selector
+click_english = driver.find_element(By.ID, 'langSelect-EN')
+driver.execute_script('arguments[0].click();', click_english)
 
-
+# click the cookie
+click_cookie = driver.find_element(By.ID, 'bigCookie')
+click_cookie.click()
 
 
 
 input("Press Enter to close the browser...")
 
 driver.quit()
+
+
